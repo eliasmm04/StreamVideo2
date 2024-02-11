ca9a261be3a710f944810e71e11c802415db3614
AlejandroRC-Github <145007287+AlejandroRC-Github@users.noreply.github.com>
2024-02-11 16:38:21 +0100

    Update views.py

--- restApi/StreamVideo/views.py
+++ restApi/StreamVideo/views.py
@@ -47,26 +47,26 @@ def PostPeliculaComentarios(request, peliculaId):
 
 @crsf_exempt
 
-def PostSeriesComentarios (request, serieId ):
+def devolver_comentarios_series (request, serieId ):
         if request.method == 'POST':
-        session_token = request.headers.get('SesionToken')
-        usuario = Users.objects.filter(sesiontoken=session_token).first()
-        if not usuario:
-        return JsonResponse({'error':'Unauthorized'}, status=401)
+                session_token = request.headers.get('SesionToken')
+                usuario = Users.objects.filter(sesiontoken=session_token).first()
+                if not usuario:
+                return JsonResponse({'error':'Unauthorized'}, status=401)
         try
                 json_peticion = json.loads(request.body)
-        nuevo_comentario = json_peticion.get('nuevo comentario')
+                nuevo_comentario = json_peticion.get('nuevo comentario')
+        
+                if not nuevo_comentario:
+                        return JsonResponse({'error':'falta el nuevo comentario en la solicitud'},estatus=400)
 
-        if not nuevo_comentario:
-                return JsonResponse({'error':'falta el nuevo comentario en la solicitud'},estatus=400)
+                serie = Tseries.objects.get(id=serie.id)
+                comentario = Tcomentarios(comentario=nuevo_comentario, serie=serie)
+                comentario.save()
 
-        serie = Tseries.objects.get(id=serie.id)
-        comentario = Tcomentarios(comentario=nuevo_comentario, serie=serie)
-        comentario.save()
+                comentarios_actualizados = list(serie.tcomentarios_set.values('id','comentario'))
 
-        comentarios_actualizados = list(serie.tcomentarios_set.values('id','comentario'))
-
-        return JsonResponse({'status':'ok','comentarios':comentarios_actualizados},status=201)
+                return JsonResponse({'status':'ok','comentarios':comentarios_actualizados},status=201)
 
         except json.JSONDecodeError:
                 return JsonResponse({'error':'Error al decodificar el Json'}, status=400)
@@ -74,6 +74,8 @@ def PostSeriesComentarios (request, serieId ):
                 return JsonResponse({'error':'La serie no existe'}, status=404)
         except Exception as e:
                 return JsonResponse({'error': str(e)} ,status=500)
+
+
 
 
 
