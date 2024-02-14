b2bc2042b926e436e47c8dde658b5af261678bb7
rubenvf14 <145007165+rubenvf14@users.noreply.github.com>
2024-02-14 18:04:06 +0100

    Actualización final de views.py

--- restApi/StreamVideo/views.py
+++ restApi/StreamVideo/views.py
@@ -6,16 +6,7 @@ from .models import Favoritos
 from .models import Series
 from django.views.decorators.csrf import csrf_exempt
 
-def devolver_peliculas(request):
-	lista = Peliculas.objects.all()
-	respuesta_final = []
-	for fila_sql in lista:
-		diccionario = {}
-		diccionario['id'] = fila_sql.id
-		diccionario['nombre'] = fila_sql.nombre
-		diccionario['genero'] = fila_sql.genero
-		respuesta_final.append(diccionario)
-	return JsonResponse(respuesta_final, safe=False)
+#He borrado el get de películas ya que pertenecía a la rama de elías
 
 # get favoritos peliculas
 def devolver_peliculas_favoritas(request):
@@ -43,19 +34,9 @@ def devolver_peliculas_favoritas(request):
 		mensaje_error = {'error': 'Usuario no autenticado'}
 		return JsonResponse(mensaje_error)
 
+#He borrado el get de series ya que pertenecía a la rama de elías
 
 #get favoritos de series
-
-def devolver_series(request):
-	lista = Series.objects.all()
-	respuesta_final = []
-	for fila_sql in lista:
-		diccionario = {}
-		diccionario['id'] = fila_sql.id
-		diccionario['nombre'] = fila_sql.nombre
-		diccionario['genero'] = fila_sql.genero
-		respuesta_final.append(diccionario)
-	return JsonResponse(respuesta_final, safe=False)
 
 def devolver_series_favoritas(request):
     # Obtén el usuario actual
@@ -85,11 +66,12 @@ def devolver_series_favoritas(request):
 #Put Favoritos
 @csrf_exempt
 def favoritosview(request):
+	#Comprobamos que el método introducido en la petición sea solo PUT
 	if request.method == 'PUT':
-                # Obtener el session token del encabezado
+                # Obtener el session token del encabezado. Quizás, sería mas óptimo utilizar request.META.get('HTTP_AUTHORIZATION', None) para obtener el sessionToken
 		session_token = request.headers.get('SessionToken')
 
-                # Verificar la sesión del usuario
+                # Verificar la sesión del usuario. Además, otra mejora que se podría implementar sería cambiar el nombre de los tokens para que no sean tan confusos
 		usuario = Users.objects.filter(sessiontoken=session_token).first()
 		if not usuario:
 			return JsonResponse({'error': 'Unauthorized'}, status=401)
@@ -104,6 +86,7 @@ def favoritosview(request):
         # Verificar si se proporciona id de película o serie
 		if id_pelicula:
 			pelicula = Peliculas.objects.filter(id=id_pelicula).first()
+			#Si no existe la película
 			if not pelicula:
 				return JsonResponse({'error': 'Not Found'}, status=404)
 
@@ -111,10 +94,12 @@ def favoritosview(request):
 			Favoritos.objects.update_or_create(
 				peliculaid=pelicula,
 				userid=usuario,
+				#Este es el objeto que cambiaremos
 				defaults={'esfavorito': es_favorito}
 			)
 		elif id_serie:
 			serie = Series.objects.filter(id=id_serie).first()
+			#Si no existe la serie
 			if not serie:
 				return JsonResponse({'error': 'Not Found'}, status=404)
 
@@ -122,6 +107,7 @@ def favoritosview(request):
 			Favoritos.objects.update_or_create(
 				serieid=serie,
 				userid=usuario,
+				#Este es el objeto que cambiaremos
 				defaults={'esfavorito': es_favorito}
 			)
 		else:
@@ -131,21 +117,25 @@ def favoritosview(request):
 	else:
 		return JsonResponse({'error': 'Method Not Allowed'}, status=405)
 
-#Logaut 
+#Logout 
 
 @csrf_exempt
 def logout(request, user_id):
-    # Comprobación de token
+    # Comprobación de token. El error_response guarda la información que le proporciona el return del verify_token
 	error_response, payload = verify_token(request)
+	#Si existe el error se visualizará por pantalla
 	if error_response:
 		return error_response
 
+	#Si el método introducido no es un PATCH, saltará el error
 	if request.method != 'PATCH':
 		return JsonResponse({'error': 'Método no permitido'}, status=405)
 
 	try:
+		#Cogemos todos los objetos del modelo Users que coincidan con el filtrado introducido
 		user = Users.objects.get(pk=user_id)
 		user.sessiontoken = None  # o user.session_token =
+		#Guardamos el objeto sessiontoken con el valor Nulo y luego guardamos los cambios con user.save()
 		user.save()
 		return JsonResponse({'message': 'Sesión cerrada exitosamente'}, status=200)
 	except Users.DoesNotExist:
